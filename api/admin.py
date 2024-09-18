3f175e2425192997a65cca66d94ef1dec25b560d
Hossein <hossein.gasemzadeh03@gmail.com>
2024-09-18 16:47:02 +0330

    feat: add impprot_export feature

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
 from .models import *
+from import_export.admin import ImportExportActionModelAdmin
 
 
-@admin.register(Transactions)
-class TransactionsAdmin(admin.ModelAdmin):
-    list_display = ['owner_address', 'to_address', 'amount']
-    list_per_page = 30
+admin.site.register(Block, ImportExportActionModelAdmin)
+admin.site.register(Transactions, ImportExportActionModelAdmin)
+
+
+# @admin.register(Transactions)
+# class TransactionsAdmin(admin.ModelAdmin):
+#     list_display = ['owner_address', 'to_address', 'amount']
+#     list_per_page = 30
     
-    search_fields = ['owner_address', 'to_address']
+#     search_fields = ['owner_address', 'to_address']
     
