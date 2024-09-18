1e7c6190caf92a5ff89c1f8433376e121d4f1131
Hossein <hossein.gasemzadeh03@gmail.com>
2024-09-18 16:29:15 +0330

    feat: add models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Transactions)
+class TransactionsAdmin(admin.ModelAdmin):
+    list_display = ['owner_address', 'to_address', 'amount']
+    list_per_page = 30
+    
+    search_fields = ['owner_address', 'to_address']
+    
--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Block(models.Model):
+    block_id = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.block_id)
+    
+
+class Transactions(models.Model):
+    block = models.ForeignKey(Block, on_delete=models.CASCADE, related_name='transactions')
+    owner_address = models.CharField(max_length=128, verbose_name='from')
+    to_address = models.CharField(max_length=128)
+    amount = models.DecimalField(default=0)
+    
+    
+    def __str__(self):
+        return f'{self.owner[:6] - self.to[:6] : self.amount}'
+    
+    
