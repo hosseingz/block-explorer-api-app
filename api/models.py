edb698bd89eb73de4c2936a3b2172721fe3af9df
Hossein <hossein.gasemzadeh03@gmail.com>
2024-09-19 09:44:21 +0330

    feat: added fields to models and updated dataset

--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Block(models.Model):
     block_id = models.PositiveIntegerField()
+    timestamp = models.PositiveIntegerField()
 
     def __str__(self):
         return str(self.block_id)
@@ -13,10 +14,10 @@ class Transactions(models.Model):
     block = models.ForeignKey(Block, on_delete=models.CASCADE, related_name='transactions')
     owner_address = models.CharField(max_length=128, verbose_name='from')
     to_address = models.CharField(max_length=128)
-    amount = models.DecimalField(default=0)
-    
+    amount = models.DecimalField(default=0, max_digits=30, decimal_places=2)
+    txID = models.CharField(max_length=64)
     
     def __str__(self):
-        return f'{self.owner_address[:8] - self.to_address[:8] : self.amount}'
+        return f'{self.owner_address[:8]} - {self.to_address[:8]} : {self.amount}'
     
     
